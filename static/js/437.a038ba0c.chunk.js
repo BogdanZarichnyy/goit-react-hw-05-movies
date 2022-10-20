0aa6b31821ebdb827e67be1d71f0d077b6100270
BogdanZarichnyy <107411214+BogdanZarichnyy@users.noreply.github.com>
2022-10-20 16:37:41 +0000

    Deploying to gh-pages from @ BogdanZarichnyy/goit-react-hw-05-movies@776163415f1c3f7329ca0d642877c0115ec1fd5c 🚀

---
+++ static/js/437.a038ba0c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[437],{437:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r="PageNotFound_title__8olfF",a=n(184),o=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("div",{className:r,children:"Page Not Found"})})}}}]);
+//# sourceMappingURL=437.a038ba0c.chunk.js.map
